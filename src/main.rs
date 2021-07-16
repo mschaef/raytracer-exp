6296328c3bf9f328dd6802b4da5b922ffcae5c7f
Mike Schaeffer <mike@mschaef.com>
2021-07-16 15:57:04 -0400

    Separtely render all three sample images on each run.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 extern crate image;
 
+use crate::image::GenericImage;
+
 const EPSILON: f64 = 0.0001;
 
 type Point = (f64, f64, f64);
@@ -281,20 +283,9 @@ fn scene_axis_spheres() -> Scene {
     }
 }
 
-fn main() {
-    let imgx = 800;
-    let imgy = 800;
-
-    let c = Camera {
-        location: (0.0, 10.0, 0.0),
-        point_at: (0.0, 0.0, 0.0),
-        u: (10.0, 0.0, 0.0),
-        v: (0.0, 0.0, -10.0)
-    };
-
-    let scene = scene_sphere_occlusion_test();
-    //let scene = scene_one_sphere();
-    //let scene = scene_axis_spheres();
+fn render(
+    c: &Camera, scene: &Scene, imgx: u32, imgy: u32
+) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
 
     let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
 
@@ -303,5 +294,38 @@ fn main() {
         *pixel = image::Rgb(to_png_color(&ray_color(&ray, &scene)));
     }
 
-    imgbuf.save("render.png").unwrap();
+    imgbuf
+}
+
+fn main() {
+    let imgdim = 1024;
+
+    let c = Camera {
+        location: (0.0, 10.0, 0.0),
+        point_at: (0.0, 0.0, 0.0),
+        u: (10.0, 0.0, 0.0),
+        v: (0.0, 0.0, -10.0)
+    };
+
+    let mut output_imgbuf = image::ImageBuffer::new(imgdim, imgdim);
+
+    let scene = [
+        scene_sphere_occlusion_test(),
+        scene_one_sphere(),
+        scene_axis_spheres()
+    ];
+
+    output_imgbuf.copy_from(&render(&c, &scene[0], imgdim / 2, imgdim / 2), 0, 0)
+        .map_err(|err| println!("{:?}", err)).ok();
+    output_imgbuf.copy_from(&render(&c, &scene[1], imgdim / 2, imgdim / 2), imgdim / 2, 0)
+        .map_err(|err| println!("{:?}", err)).ok();
+    output_imgbuf.copy_from(&render(&c, &scene[2], imgdim / 2, imgdim / 2), 0, imgdim / 2)
+        .map_err(|err| println!("{:?}", err)).ok();
+
+    for ii in 0..imgdim - 1 {
+        *output_imgbuf.get_pixel_mut(ii, imgdim / 2) = image::Rgb([255, 255, 255]);
+        *output_imgbuf.get_pixel_mut(imgdim / 2, ii) = image::Rgb([255, 255, 255]);
+    }
+
+    output_imgbuf.save("render.png").unwrap();
 }
